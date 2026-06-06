72fed3e5b83a70ea468478927413f43bf28eabd4
Sudhir Kumar Alwala <sudhir.22sep@gmail.com>
2026-06-06 13:01:21 +0000

    Update site branding to amma collectivewear and add Pages custom domain CNAME

--- Nizam/src/app/app.ts
+++ Nizam/src/app/app.ts
@@ -9,5 +9,5 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('nizam.ai');
+  protected readonly title = signal('amma collectivewear');
 }
