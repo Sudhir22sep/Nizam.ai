ddc59b4fd97c0e0462de942b1705f60274c4eecf
Sudhir Kumar Alwala <sudhir.22sep@gmail.com>
2026-07-22 19:51:42 +0000

    server ts code changes for the eroro catcha nd small change

--- Nizam/src/server.ts
+++ Nizam/src/server.ts
@@ -575,13 +575,13 @@ app.use(
 /**
  * Handle all other requests by rendering the Angular application.
  */
-app.use((req, res, next) => {
-  angularApp
-    .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
-    .catch(next);
+app.use(async (req, res, next) => {
+  try {
+    const response = await angularApp.handle(req);
+    response ? writeResponseToNodeResponse(response, res) : next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 /**
